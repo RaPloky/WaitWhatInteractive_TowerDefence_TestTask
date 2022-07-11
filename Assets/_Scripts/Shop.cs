bd33301a81f2eb35c54631e06b45fe5dfe74b4c8
RaPloky <raplokygamingone@gmail.com>
2022-07-11 20:04:33 +0300

    Tower cost text shows proper cost now

--- Assets/_Scripts/Shop.cs
+++ Assets/_Scripts/Shop.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class Shop : MonoBehaviour
 {
     public Blueprint bulletTower;
     public Blueprint energyTower;
 
+    [SerializeField] TextMeshProUGUI bulletTowerCost;
+    [SerializeField] TextMeshProUGUI energyTowerCost;
+
     private BuildManager _buildManager;
 
     private void Start()
     {
         _buildManager = BuildManager.instance;
+        bulletTowerCost.text = "$" + bulletTower.towerCosts[0];
+        energyTowerCost.text = "$" + energyTower.towerCosts[0];
     }
     public void SelectBulletTower()
     {
